5bd5a8fdbb043b8c93236f3b22386d2fc307b95d
EllenSampaio <ellenssbarross@gmail.com>
2022-07-21 19:11:52 -0300

    adiciona exercicios de JS

---
+++ 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JavaScript - Primeiros passos/scripts.js
@@ -0,0 +1,20 @@
+
+let nota = "200";
+
+
+switch (nota) {
+  case "200":
+    console.log("aprovada");
+    break
+case "100":
+  console.log("lista");
+  break
+
+case "50":
+  console.log("reprovada");
+  break
+
+  default:
+    console.log("nao identificado");
+}
+
