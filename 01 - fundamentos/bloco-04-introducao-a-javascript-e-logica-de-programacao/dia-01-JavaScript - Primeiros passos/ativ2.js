72689d69201c201aa27efb38b8cdc603d33d4840
EllenSampaio <ellenssbarross@gmail.com>
2022-07-21 19:12:50 -0300

    adiciona exercicios de JS

---
+++ 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JavaScript - Primeiros passos/ativ2.js
@@ -0,0 +1,8 @@
+const a = 130;
+const b = 20;
+
+if(a > b) {
+  console.log('"a" é o maior de dois numeros');
+}else {
+  console.log('"b" é maior que A');
+}
---
+++ 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JavaScript - Primeiros passos/ativ4.js
@@ -0,0 +1,11 @@
+const a = 130;
+const b = 20;
+const c = 600;
+
+if(a > b && a > c) {
+  console.log('"a" é o maior de tres numeros');
+}  else if (b > a && b > c) {
+  console.log('"b" é maior de tres numeros');
+} else if(c > a && c > b) {
+  console.log('"c" é o maior de tres numeros')
+}
