9271c425248a69530311656d585e5d0da7ae6e95
EllenSampaio <ellenssbarross@gmail.com>
2022-07-23 15:28:28 -0300

    Adiciona exercicios e bonus

--- 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-JavaScript-array-e-loopfor/exercicio6.js
+++ 01 - fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-JavaScript-array-e-loopfor/exercicio6.js
@@ -1,1 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numberOdd = 0;
+
+for (let index = 0; index < numbers.length; index += 1) 
+{
+  if (numbers[index] % 2 == 0) {
+    numberOdd = numberOdd + 1;
+  }
+}
+
+if (numberOdd == 0) {
+  console.log("nenhum valor ímpar encontrado");
+} else {
+  console.log(numberOdd);
+}
